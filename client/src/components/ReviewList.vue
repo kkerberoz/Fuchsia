a17d063714d58c1f1df12fff3813900e0467b207
damorz <dive2080@gmail.com>
2020-10-28 16:57:17 +0700

    WIP: home card

---
+++ client/src/components/ReviewList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container is-fullhd" v-if="hasReview">
+        <div v-for="review in 10" :key="review" class="horizontal-container">
+            <review-item :review="review"></review-item>
+        </div>
+    </div>
+</template>
+
+<script>
+import ReviewList from "./ReviewItem";
+export default {
+    components: {
+        'review-item': ReviewList
+    },
+    computed: {
+        hasReview() {
+            return true;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .horizontal-container {
+        display: flex;
+        overflow-x:auto;
+    }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-  <section class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-      </div>
+  <section class="hero-body is-fullheight">
+    <div class="container">
+      <review-list></review-list>
     </div>
   </section>
 </template>
 
 <script>
+import ReviewList from "../components/ReviewList";
   export default {
     name: "Home",
+    components: {
+      'review-list': ReviewList
+    }
   };
 </script>
