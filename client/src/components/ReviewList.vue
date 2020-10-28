f393287e300f1d7d3dad01510aa78b6a7d25bb2b
damorz <dive2080@gmail.com>
2020-10-28 18:07:45 +0700

    WIP: Home card 2

--- client/src/components/ReviewList.vue
+++ client/src/components/ReviewList.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="container is-fullhd" v-if="hasReview">
-        <div v-for="review in 10" :key="review" class="horizontal-container">
-            <review-item :review="review"></review-item>
-        </div>
+  <div class="columns is-mobile is-multiline is-centered" v-if="hasReview">
+    <div v-for="review in 10" :key="review" class="column is-narrow">
+        <review-item :review="review"></review-item>
     </div>
+  </div>
 </template>
 
 <script>
+// <review-item :review="review"></review-item>
 import ReviewList from "./ReviewItem";
 export default {
-    components: {
-        'review-item': ReviewList
+  components: {
+    "review-item": ReviewList,
+  },
+  computed: {
+    hasReview() {
+      return true;
     },
-    computed: {
-        hasReview() {
-            return true;
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-    .horizontal-container {
-        display: flex;
-        overflow-x:auto;
-    }
+.horizontal-container {
+  display: flex;
+  overflow-x: auto;
+}
 </style>
