2bc580d51f9126a0324642d6042e698e9e9b28a4
Krittaphon Raksakul <42891316+kkerberoz@users.noreply.github.com>
2020-11-13 14:48:25 +0700

    Mhee (#1)
    * Add tiptap button svg
    
    * Add tiptap
    
    * Add component for import svg icon tiptap
    
    * Add tiptap to page
    
    * Fix frontend ci
    
    * Update frontend CI

--- client/src/views/CreateReview.vue
+++ client/src/views/CreateReview.vue
@@ -26,78 +26,157 @@
     <div id="postBlog" class="container">
       <div class="columns">
         <div id="reviewContent" class="column">
-          <div v-if="isSelectCategoryState">
-            <div class="columns">
-              <div class="column is-3">
-                <label class="checkbox">
-                <input v-model="Camera" value="Camera" type="checkbox">
-                Camera
-              </label>
-              </div>
-              <div class="column is-3">
-                <label class="checkbox">
-                  <input v-model="Natural" type="checkbox">
-                  Natural
-                </label>
-              </div>
-              <div class="column is-3">
-                <label class="checkbox">
-                  <input v-model="Shopping" type="checkbox">
-                  Shopping
-                </label>
-              </div>
-              <div class="column is-3">
-                <label class="checkbox">
-                    <input v-model="Food" type="checkbox">
-                    Food
-                </label>
-              </div>
-            </div>
-            <div class="columns">
-              <div class="column is-3">
-                <label class="checkbox">
-                    <input v-model="Drink" type="checkbox">
-                    Drink
-                </label>
-              </div>
-              <div class="column is-3">
-                <label class="checkbox">
-                  <input v-model="Restaurant" type="checkbox">
-                  Restaurant
-                </label>
-              </div>
-              <div class="column is-3">
-                <label class="checkbox">
-                  <input v-model="Sports" type="checkbox">
-                  Sports
-                </label>
-              </div>
-            </div>
-          </div>
+          <div v-if="isSelectCategoryState"></div>
           <div v-if="isCreateReviewState">
-            
+            <div class="editor">
+              <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+                <div class="menubar">
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.bold() }"
+                    @click="commands.bold"
+                  >
+                    <icon name="bold" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.italic() }"
+                    @click="commands.italic"
+                  >
+                    <icon name="italic" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.strike() }"
+                    @click="commands.strike"
+                  >
+                    <icon name="strike" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.underline() }"
+                    @click="commands.underline"
+                  >
+                    <icon name="underline" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.code() }"
+                    @click="commands.code"
+                  >
+                    <icon name="code" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.paragraph() }"
+                    @click="commands.paragraph"
+                  >
+                    <icon name="paragraph" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+                    @click="commands.heading({ level: 1 })"
+                  >
+                    H1
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+                    @click="commands.heading({ level: 2 })"
+                  >
+                    H2
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+                    @click="commands.heading({ level: 3 })"
+                  >
+                    H3
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.bullet_list() }"
+                    @click="commands.bullet_list"
+                  >
+                    <icon name="ul" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.ordered_list() }"
+                    @click="commands.ordered_list"
+                  >
+                    <icon name="ol" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.blockquote() }"
+                    @click="commands.blockquote"
+                  >
+                    <icon name="quote" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    :class="{ 'is-active': isActive.code_block() }"
+                    @click="commands.code_block"
+                  >
+                    <icon name="code" />
+                  </button>
+
+                  <button
+                    class="menubar__button"
+                    @click="commands.horizontal_rule"
+                  >
+                    <icon name="hr" />
+                  </button>
+
+                  <button class="menubar__button" @click="commands.undo">
+                    <icon name="undo" />
+                  </button>
+
+                  <button class="menubar__button" @click="commands.redo">
+                    <icon name="redo" />
+                  </button>
+                </div>
+              </editor-menu-bar>
+
+              <editor-content class="editor__content" :editor="editor" />
+            </div>
           </div>
           <div v-if="isPreviewState">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
-            since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with
-            the release of Letraset sheets containing Lorem Ipsum passages, and
-            more recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of
-            the printing and typesetting industry. Lorem Ipsum has been the
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
             industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen
-            book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
-            since the 1500s, when an unknown printer took a galley of type and
+            printer took a galley of type and
           </div>
         </div>
       </div>
@@ -117,189 +196,224 @@
         </div>
       </div>
     </div>
+    <comment-list></comment-list>
   </div>
 </template>
 
 <script>
-export default {
-  // props: {
-  //   reviewId: {
-  //     type: String,
-  //     required: true,
-  //   },
-  // },
-  data() {
-    return {
-      tabStage: 1,
-      category: [],
-      Camera: false,
-      Natural: false,
-      Shopping: false,
-      Food: false,
-      Drink: false,
-      Restaurant: false,
-      Sports: false,
-    };
-  },
-  computed: {
-    isSelectCategoryState() {
-      return this.tabStage === 1;
+  // import ReviewContent from "../components/ReviewContent";
+  import Icon from "../components/Icon";
+  import CommentList from "../components/CommentList";
+  import { Editor, EditorContent, EditorMenuBar } from "tiptap";
+  import {
+    Blockquote,
+    CodeBlock,
+    HardBreak,
+    Heading,
+    HorizontalRule,
+    OrderedList,
+    BulletList,
+    ListItem,
+    TodoItem,
+    TodoList,
+    Bold,
+    Code,
+    Italic,
+    Link,
+    Strike,
+    Underline,
+    History,
+  } from "tiptap-extensions";
+
+  export default {
+    components: {
+      // ReviewContent,
+      CommentList,
+      EditorContent,
+      EditorMenuBar,
+      Icon,
     },
-    isCreateReviewState() {
-      return this.tabStage === 2;
+    props: {
+      reviewId: {
+        type: String,
+        required: true,
+      },
     },
-    isPreviewState() {
-      return this.tabStage === 3;
+    data() {
+      return {
+        tabStage: 1,
+        html: "",
+        content: "",
+
+        editor: new Editor({
+          extensions: [
+            new Blockquote(),
+            new BulletList(),
+            new CodeBlock(),
+            new HardBreak(),
+            new Heading({ levels: [1, 2, 3] }),
+            new HorizontalRule(),
+            new ListItem(),
+            new OrderedList(),
+            new TodoItem(),
+            new TodoList(),
+            new Link(),
+            new Bold(),
+            new Code(),
+            new Italic(),
+            new Strike(),
+            new Underline(),
+            new History(),
+          ],
+          onUpdate: ({ getHTML }) => {
+            this.html = getHTML();
+            if (this.html === "<p></p>") this.content = "";
+            else this.content = this.html;
+          },
+        }),
+      };
     },
-    goButton() {
-      return this.tabStage === 3 ? "Submit" : "Next";
+    computed: {
+      isSelectCategoryState() {
+        return this.tabStage === 1;
+      },
+      isCreateReviewState() {
+        return this.tabStage === 2;
+      },
+      isPreviewState() {
+        return this.tabStage === 3;
+      },
+      goButton() {
+        return this.tabStage === 3 ? "Submit" : "Next";
+      },
+      isFirstStep() {
+        return this.tabStage === 1;
+      },
     },
-    isFirstStep() {
-      return this.tabStage === 1;
+
+    beforeDestroy() {
+      this.editor.destroy();
     },
-  },
-  methods: {
-    selectCategory() {
-      this.tabStage = 1;
-    },
-    createReview() {
-      this.tabStage = 2;
-    },
-    preview() {
-      this.tabStage = 3;
-    },
-    next() {
-      if (this.tabStage === 3) {
-        //Go post
-        let testObj = {
-          userId: "12345",
-          reviewTitle: "test1sd",
-          reviewContent: "this is content",
-          reviewDatetime: "wednesday",
-          category: "game",
-          status: "Ban",
-          imageLink: "https://test.png",
-          view: 0
+    methods: {
+      showImagePrompt(command) {
+        const src = prompt("Enter the url of your image here");
+        if (src !== null) {
+          command({ src });
         }
-        this.$store.dispatch("review/postReview", testObj);
-      } else {
-        this.tabStage += 1;
-        //next step
-      }
+      },
+      selectCategory() {
+        this.tabStage = 1;
+      },
+      createReview() {
+        this.tabStage = 2;
+      },
+      preview() {
+        this.tabStage = 3;
+      },
+      next() {
+        if (this.tabStage === 3) {
+          //Go post
+        } else {
+          this.tabStage += 1;
+          console.log(this.content);
+          //next step
+        }
+      },
+      back() {
+        this.tabStage -= 1;
+      },
     },
-    back() {
-      this.tabStage -= 1;
+    mounted() {
+      console.log(this.reviewId);
     },
-  },
-  mounted() {
-    console.log(this.reviewId);
-  },
-  watch: {
-    Camera() {
-      this.Camera ? this.category.push("Camera") : this.category.splice(this.category.indexOf("Camera"),1);
+    watch: {
+      tabStage() {
+        if (this.tabStage === 1) {
+          document.getElementById("categoryTab").style.backgroundColor =
+            "#c6007e";
+          document.getElementById("createReviewTab").style.backgroundColor =
+            "#f277c6";
+          document.getElementById("previewTab").style.backgroundColor =
+            "#f277c6";
+        } else if (this.tabStage === 2) {
+          document.getElementById("categoryTab").style.backgroundColor =
+            "#f277c6";
+          document.getElementById("createReviewTab").style.backgroundColor =
+            "#c6007e";
+          document.getElementById("previewTab").style.backgroundColor =
+            "#f277c6";
+        } else {
+          document.getElementById("categoryTab").style.backgroundColor =
+            "#f277c6";
+          document.getElementById("createReviewTab").style.backgroundColor =
+            "#f277c6";
+          document.getElementById("previewTab").style.backgroundColor =
+            "#c6007e";
+        }
+      },
     },
-    Natural() {
-      this.Natural ? this.category.push("Natural") : this.category.splice(this.category.indexOf("Natural"),1);
-    },
-    Shopping() {
-      this.Shopping ? this.category.push("Shopping") : this.category.splice(this.category.indexOf("Shopping"),1);
-    },
-    Food() {
-      this.Food ? this.category.push("Food") : this.category.splice(this.category.indexOf("Food"),1);
-    },
-    Drink() {
-      this.Drink ? this.category.push("Drink") : this.category.splice(this.category.indexOf("Drink"),1);
-    },
-    Restaurant() {
-      this.Restaurant ? this.category.push("Restaurant") : this.category.splice(this.category.indexOf("Restaurant"),1);
-    },
-    Sports() {
-      this.Sports ? this.category.push("Sports") : this.category.splice(this.category.indexOf("Sports"),1);
-    },
-    tabStage() {
-      if (this.tabStage === 1) {
-        document.getElementById("categoryTab").style.backgroundColor =
-          "#c6007e";
-        document.getElementById("createReviewTab").style.backgroundColor =
-          "#f277c6";
-        document.getElementById("previewTab").style.backgroundColor = "#f277c6";
-      } else if (this.tabStage === 2) {
-        document.getElementById("categoryTab").style.backgroundColor =
-          "#f277c6";
-        document.getElementById("createReviewTab").style.backgroundColor =
-          "#c6007e";
-        document.getElementById("previewTab").style.backgroundColor = "#f277c6";
-      } else {
-        document.getElementById("categoryTab").style.backgroundColor =
-          "#f277c6";
-        document.getElementById("createReviewTab").style.backgroundColor =
-          "#f277c6";
-        document.getElementById("previewTab").style.backgroundColor = "#c6007e";
-      }
-    },
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.buttonGroup {
-  display: flex;
-  width: 100%;
-  justify-content: flex-end;
-}
+  .buttonGroup {
+    display: flex;
+    width: 100%;
+    justify-content: flex-end;
+  }
 
-.buttonControl {
-  text-align: center;
-  background-color: $primary;
-  border-radius: 50px;
-  color: white;
-  cursor: pointer;
-  margin-right: 1%;
-}
-
-#reviewContent {
-  margin: 3%;
-}
-
-#tabGroup {
-  margin-bottom: 0;
-  margin-left: 5%;
-}
-
-#categoryTab {
-  background-color: $primary;
-}
-
-.tabStage {
-  text-align: center;
-  color: white;
-  margin-top: 5%;
-  border-color: white;
-  height: 6vh;
-  margin-left: -5%;
-  border-top-left-radius: 50px;
-  border-top-right-radius: 30px;
-  background-color: $secondary;
-  cursor: pointer;
-}
-
-#postBlog {
-  padding: 40px;
-  background-image: url("../assets/svg/postBlog.svg#svgView(preserveAspectRatio(none))");
-  background-size: cover;
-  width: 100%;
-  height: auto;
-}
-
-@media screen and (max-width: 1024px) {
-  #postBlog {
-    padding: 20px;
-    margin: 5px;
+  .buttonControl {
+    text-align: center;
+    background-color: $primary;
     border-radius: 50px;
+    color: white;
+    cursor: pointer;
+    margin-right: 1%;
+  }
+
+  #reviewContent {
+    margin: 3%;
+  }
+
+  #tabGroup {
+    margin-bottom: 0;
+    margin-left: 5%;
+  }
+
+  #categoryTab {
+    background-color: $primary;
+  }
+
+  .tabStage {
+    text-align: center;
+    color: white;
+    margin-top: 5%;
+    border-color: white;
+    height: 6vh;
+    margin-left: -5%;
+    border-top-left-radius: 50px;
+    border-top-right-radius: 30px;
+    background-color: $secondary;
+    cursor: pointer;
+  }
+
+  #postBlog {
+    padding: 40px;
+    background-image: url("../assets/svg/postBlog.svg#svgView(preserveAspectRatio(none))");
+    background-size: cover;
     width: 100%;
     height: auto;
-    background-image: none;
-    background-color: white;
   }
-}
+
+  @media screen and (max-width: 1024px) {
+    #postBlog {
+      padding: 20px;
+      margin: 5px;
+      border-radius: 50px;
+      width: 100%;
+      height: auto;
+      background-image: none;
+      background-color: white;
+    }
+  }
 </style>
