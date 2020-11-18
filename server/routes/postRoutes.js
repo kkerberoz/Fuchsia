4bbc4a9a0040ca34f9236fc2f440288df6a38237
Wasurat Chaiyaphumsakun <ratwc@Wasurats-MacBook-Pro.local>
2020-11-18 22:24:20 +0700

    delete review

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,12 +7,14 @@ const PostController = require("../controller/postController");
 //Routes
 router.post("/postreview", auth, PostController.postReview);
 router.get("/getreview", PostController.getReviews);
+router.get("/deletereview", PostController.deleteReview);
 router.get("/getreviewscount", PostController.getReviewsCount);
 router.get("/getreviewinfo", PostController.getReviewInfo);
 // router.post("/postcomment", auth, PostController.postComment);
 // router.get("/getcomment", PostController.getComments);
 // router.get("/getfollowed", PostController.getFolloweds);
 // router.get("/getfavorite", PostController.getFavorite);
+router.get("/violentregconition", PostController.violentRegconition);
 
 
 module.exports = router;
