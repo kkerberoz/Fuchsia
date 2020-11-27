dd8d90656eb04db4bd5605121d9e6ce920ab3d13
Wasurat Chaiyaphumsakun <ratwc@Wasurats-MacBook-Pro.local>
2020-11-27 15:56:00 +0700

    getViolent

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -320,8 +320,7 @@ module.exports = {
         try {
           const reviews = await Review.find({ _id: violents[i].reviewId }, { _id: 0, reviewTitle: 1, category: 1, reviewDatetime: 1, userId: 1 })
           const users = await User.find({ _id: reviews[0].userId }, { _id: 0, username: 1 })
-          Object.assign(reviews[0], {username: users[0].username})
-          violentDetails.push( { "reviewDetails": reviews[0], "violentContent": violents[i]});
+          violentDetails.push( { "userDetails": users[0], "reviewDetails": reviews[0], "violentContent": violents[i]});
         } catch (err) {
           ResHelper.error(res, err)
         }
